526aab3175e5e0cc3d59951f31013aabc33e3102
HamletAghanyan <129510622+HamletAghanyan@users.noreply.github.com>
2023-03-31 16:39:46 +0400

    Added max_of_3 implementation

--- max_of_3/max_of_3.py
+++ max_of_3/max_of_3.py
@@ -1,1 +1,13 @@
 print('Hello, world!')
+a = int(input("Enter A -> "))
+b = int(input("Enter B -> "))
+c = int(input("Enter C -> "))
+if (a > b):
+    if (a > c):
+        print (a)
+    else:
+        print(c)
+elif (b > c):
+     print (b)
+else:
+     print (c)
