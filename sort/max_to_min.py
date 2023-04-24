4d33e1347207a66be34c9b1c08080f376f510752
HamletAghanyan <129510622+HamletAghanyan@users.noreply.github.com>
2023-04-24 15:18:27 +0400

    Create max_to_min.py
    added sorting algorithm

---
+++ sort/max_to_min.py
@@ -0,0 +1,20 @@
+print("Enter 3 numbers and get the max one!")
+a = int(input("Enter A -> "))
+b = int(input("Enter B -> "))
+c = int(input("Enter C -> "))
+if (a > b):
+    if (a > c):
+        if (c > b):
+            print (a,c,b) 
+        else:
+            print(a,b,c)
+    else:
+        print (c,a,b)
+else:
+    if (b > c):
+        if (c > a):  
+            print (b,a,c)
+        else:
+            print (b,c,a)
+    else:
+        print (c,b,a)
